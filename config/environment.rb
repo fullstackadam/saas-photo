5e0c9fa4ffdeb3e00350a39bd412e31345b13993
Adam McMahon <mcmahonempire@gmail.com>
2017-11-14 19:59:09 -0700

    add: setup email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :api_key => ENV['SENDGRID_API_KEY'],
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
